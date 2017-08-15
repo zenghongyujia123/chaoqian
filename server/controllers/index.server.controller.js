4876f3c9083b1192a71f56161c7e91b54313a64b
zenghongyujia123 <1963968619@qq.com>
2017-08-15 20:09:34 +0800

    Update product list style

--- server/controllers/index.server.controller.js
+++ server/controllers/index.server.controller.js
@@ -4,7 +4,7 @@
 var path = require('path');
 
 exports.index = function (req, res, next) {
-  var filepath = path.join(__dirname, '../../web/site_platform/views/index.client.view.html');
+  var filepath = path.join(__dirname, '../../web/site_platform/views/home.client.view.html');
   console.log(filepath);
   return res.sendFile(filepath);
 };
