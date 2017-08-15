9e324227d4691c4fc1f7e4b6e5da1ab096b53983
zenghongyujia123 <1963968619@qq.com>
2017-08-15 16:22:09 +0800

    Add link for product list

--- server/routes/index.server.route.js
+++ server/routes/index.server.route.js
@@ -6,4 +6,6 @@ var index = require('../controllers/index');
 
 module.exports = function (app) {
   app.route('/').get(index.index);
+  app.route('/product_list').get(index.product_list);
+  app.route('/product_detail').get(index.product_detail);
 };
