9685b7fc80b6a94e21d1b7b6c518885dd41928a0
zenghongyujia123 <1963968619@qq.com>
2017-11-05 00:59:24 +0800

    Test

--- server/controllers/carrier.server.controller.js
+++ server/controllers/carrier.server.controller.js
@@ -26,7 +26,7 @@ exports.page_carrier_callback = function (req, res, next) {
 };
 
 exports.page_carrier_url = function (req, res, next) {
-  agent.get('http://e.apix.cn/apixanalysis/mobile/yys/phone/carrier/page?success_url=http://chaoqianwang.com/carrier/page_success&failed_url=http://chaoqianwang.com/carrier/page_failed&callback_url=http://chaoqianwang.com/carrier/page_carrier_callback')
+  agent.get('http://e.apix.cn/apixanalysis/mobile/yys/phone/carrier/page?success_url=http://chaoqianwang.com/carrier/page_carrier_success?userid=' + user._id + '&failed_url=http://chaoqianwang.com/carrier/page_carrier_failed&callback_url=http://chaoqianwang.com/carrier/page_carrier_callback')
     .set('apix-key', '92fd3f3bf03a40087fe4ece5bba355cf')
     .set('content-type', 'application/json')
     .set('accept', 'application/json')
--- server/routes/carrier.server.route.js
+++ server/routes/carrier.server.route.js
@@ -3,10 +3,11 @@
  */
 
 var carrierController = require('../controllers/carrier');
+var userFilter = require('../filters/user');
 
 module.exports = function (app) {
-  app.route('/carrier/page_success').get(carrierController.page_carrier_success);
-  app.route('/carrier/page_carrier_failed').get(carrierController.page_carrier_failed);
-  app.route('/carrier/page_carrier_callback').get(carrierController.page_carrier_callback);
-  app.route('/carrier/page_carrier_url').get(carrierController.page_carrier_url);
+  app.route('/carrier/page_carrier_success').get(userFilter.requireUser, carrierController.page_carrier_success);
+  app.route('/carrier/page_carrier_failed').get(userFilter.requireUser, carrierController.page_carrier_failed);
+  app.route('/carrier/page_carrier_callback').get(userFilter.requireUser, carrierController.page_carrier_callback);
+  app.route('/carrier/page_carrier_url').get(userFilter.requireUser, carrierController.page_carrier_url);
 };
