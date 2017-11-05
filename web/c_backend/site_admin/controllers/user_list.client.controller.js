26409aba942deb36b0af03b16f5045a8c3fde42c
zenghongyujia123 <1963968619@qq.com>
2017-11-06 02:58:57 +0800

    Update for test

--- web/c_backend/site_admin/controllers/user_list.client.controller.js
+++ web/c_backend/site_admin/controllers/user_list.client.controller.js
@@ -26,10 +26,10 @@ cSite.controller('UserListController', [
     $scope.getVipStatus = function (status) {
       var map = {
         'un_submit': {
-          text: '未递交'
+          text: '未递交材料'
         },
         'submit': {
-          text: '已递交'
+          text: '已递交材料'
         },
         'passed': {
           text: '审核通过'
