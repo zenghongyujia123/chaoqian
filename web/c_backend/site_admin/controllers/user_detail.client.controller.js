f91b882fdbe7f4a5983bd30fc2f2949fba2921d6
zenghongyujia123 <1963968619@qq.com>
2017-11-06 23:50:19 +0800

    Update for test

--- web/c_backend/site_admin/controllers/user_detail.client.controller.js
+++ web/c_backend/site_admin/controllers/user_detail.client.controller.js
@@ -14,6 +14,12 @@ cSite.controller('UserDetailController', [
       UserNetwork.getUserById($scope, { user_id: $stateParams.user_id }).then(function (data) {
         console.log(data);
         if (!data.err) {
+          if (data.carrier_detail) {
+            data.carrier_detail = JSON.parse(data.carrier_detail);
+          }
+          if (data.pbc_detail) {
+            data.pbc_detail = JSON.parse(data.pbc_detail);
+          }
           $scope.user = data;
         }
       }, function (err) {
