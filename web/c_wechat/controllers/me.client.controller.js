044915a5ad7a13251276ad96b832e52307588206
zenghongyujia123 <1963968619@qq.com>
2017-11-15 00:13:19 +0800

    Update for test

--- web/c_wechat/controllers/me.client.controller.js
+++ web/c_wechat/controllers/me.client.controller.js
@@ -2,7 +2,7 @@ $(function () {
   var vipTime = $('.vip-time').text();
   if (vipTime) {
     var start = new Date(vipTime);
-    var end = new Date(vipTime).setMonth(new Date(vipTime).getMonth() + 2);
+    var end = new Date(new Date(vipTime).setMonth(new Date(vipTime).getMonth() + 2));
     var date = getDurationDays(start, end);
     $('.vip-time').text('您的会员将在' + date + '后失效');
   }
--- web/c_wechat/helpers/time.client.helper.js
+++ web/c_wechat/helpers/time.client.helper.js
@@ -13,5 +13,5 @@ function getDurationDays(date1, date2) {
   //计算相差秒数  
   var leave3 = leave2 % (60 * 1000)      //计算分钟数后剩余的毫秒数  
   var seconds = Math.round(leave3 / 1000)
-  return "" + days + "天 " + hours + "小时 " + minutes + " 分钟" + seconds + " 秒";
+  return "" + days + "天 ";// + hours + "小时 " + minutes + " 分钟" + seconds + " 秒";
 }
