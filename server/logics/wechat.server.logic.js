b32e83da09a57723d0294fd448a79fa544307f71
zenghongyujia123 <1963968619@qq.com>
2017-11-23 00:48:14 +0800

    Update for test

--- server/logics/wechat.server.logic.js
+++ server/logics/wechat.server.logic.js
@@ -53,7 +53,7 @@ exports.getUserJsApiTicket = function (callback) {
           'jsapi_ticket=' + ticket,
           'noncestr=' + noncestr,
           'timestamp=' + timestamp,
-          'url=http://chaoqianwang.com/page_wechat/home'
+          'url=http://chaoqianwang.com/page_wechat/self_home'
         ];
         str = str.sort().join('&');
         console.log(str);
