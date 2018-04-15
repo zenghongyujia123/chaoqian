86be713d08662c4654d69c00213f1903cbb76674
hardy <hardy@hardydeMacBook-Pro.local>
2018-04-15 17:54:51 +0800

    Update for test

--- server/routes/wechat_pay.server.route.js
+++ server/routes/wechat_pay.server.route.js
@@ -7,7 +7,7 @@ var userFilter = require('../filters/user');
 
 module.exports = function (app) {
   // app.route('/page_wechat/getPrePayId').post(userFilter.requireUser, payController.getPrePayId);
-  app.route('/page_wechat/getPrePayId4PayCredit').post(userFilter.requireUser, payController.getPrePayId4PayCredit);
+  // app.route('/page_wechat/getPrePayId4PayCredit').post(userFilter.requireUser, payController.getPrePayId4PayCredit);
   ///page_wechat/getPrePayId4PayCredit
   // app.route('/page_wechat/getPayPage').get(userFilter.requireUser, payController.getPayPage);
   // app.route('/page_wechat/getUserJsApiTicket').post(payController.getUserJsApiTicket);
