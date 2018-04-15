baf1ca5c4ade521123e05c080bc7de9858329b07
hardy <hardy@hardydeMacBook-Pro.local>
2018-04-15 17:02:52 +0800

    Update for test

--- server/routes/wechat_pay.server.route.js
+++ server/routes/wechat_pay.server.route.js
@@ -6,10 +6,10 @@ var payController = require('../controllers/wechat_pay');
 var userFilter = require('../filters/user');
 
 module.exports = function (app) {
-  app.route('/page_wechat/getPrePayId').post(userFilter.requireUser, payController.getPrePayId);
+  // app.route('/page_wechat/getPrePayId').post(userFilter.requireUser, payController.getPrePayId);
   app.route('/page_wechat/getPrePayId4PayCredit').post(userFilter.requireUser, payController.getPrePayId4PayCredit);
   ///page_wechat/getPrePayId4PayCredit
-  app.route('/page_wechat/getPayPage').get(userFilter.requireUser, payController.getPayPage);
+  // app.route('/page_wechat/getPayPage').get(userFilter.requireUser, payController.getPayPage);
   // app.route('/page_wechat/getUserJsApiTicket').post(payController.getUserJsApiTicket);
 
   
