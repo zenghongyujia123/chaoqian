00ad75f3fa97d0ab5a974f620d8cde67dead7d81
hardy <hardy@hardydeMacBook-Pro.local>
2018-04-21 20:10:10 +0800

    Update for test

--- web/c_backend/site_admin/controllers/user_list.client.controller.js
+++ web/c_backend/site_admin/controllers/user_list.client.controller.js
@@ -5,86 +5,89 @@ cSite.controller('UserListController', [
       $state.go('user_detail', { user_id: user._id });
     }
 
-/*
-    function removeByValue(arr, val) {
-      for(var i=0; i<arr.length; i++) {
-        if(arr[i].vip_payed === val) {
-          arr.splice(i, 1);
+    /*
+        function removeByValue(arr, val) {
+          for(var i=0; i<arr.length; i++) {
+            if(arr[i].vip_payed === val) {
+              arr.splice(i, 1);
+            }
+          }
         }
-      }
-    }
- */ 
+     */
     $scope.user_list = [];
 
 
     $scope.userList = function () {
-   //   $scope.user_list = [];
+      //   $scope.user_list = [];
       UserNetwork.userList($scope, {}).then(function (data) {
         console.log(data);
         if (!data.err) {
           $scope.user_list = data;
-/*          
-          if(par===2){
-            alert(par);
-            data.map(function(value,index,array){
-              if (value.vip_payed){
-                array.splice(index, value.length);
-                //delete(value);
-              }              
-            });
-            alert(JSON.stringify(data));
-            $scope.user_list = data;
-          }
-          else if(par===1)
-          {
-            
-            //removeByValue(data,false);
-            alert(par);
-            data.map(function(value,index,array){
-              if ((!value.vip_payed)||typeof(value.vip_payed)=="undefined"){
-                array.splice(index, 1);
-                //delete(value);              
-              }              
-            });
-            alert(JSON.stringify(data));
-           // $scope.user_list = data;
-          }
-          else{
-            alert(par);
-            $scope.user_list = data;
-          }
-*/
+          /*          
+                    if(par===2){
+                      alert(par);
+                      data.map(function(value,index,array){
+                        if (value.vip_payed){
+                          array.splice(index, value.length);
+                          //delete(value);
+                        }              
+                      });
+                      alert(JSON.stringify(data));
+                      $scope.user_list = data;
+                    }
+                    else if(par===1)
+                    {
+                      
+                      //removeByValue(data,false);
+                      alert(par);
+                      data.map(function(value,index,array){
+                        if ((!value.vip_payed)||typeof(value.vip_payed)=="undefined"){
+                          array.splice(index, 1);
+                          //delete(value);              
+                        }              
+                      });
+                      alert(JSON.stringify(data));
+                     // $scope.user_list = data;
+                    }
+                    else{
+                      alert(par);
+                      $scope.user_list = data;
+                    }
+          */
 
         }
       }, function (err) {
         console.log(err);
       });
     };
-    var userListByCondition = function(condition,sort){
+    var userListByCondition = function (condition, sort) {
 
-      UserNetwork.userListByCondition($scope, {'condition': condition,'sort':sort}).then(function (data){
+      UserNetwork.userListByCondition($scope, { 'condition': condition, 'sort': sort }).then(function (data) {
         $scope.user_list = data;
       });
     }
-    
-    $scope.all=function()
-    {
+
+    $scope.all = function () {
       $scope.user_list = [];
       $scope.userList();
     }
 
-    $scope.remove_non_vip=function()
-    {
+    $scope.remove_non_vip = function () {
       $scope.user_list = [];
 
-      userListByCondition({'vip_payed':true},{'username':1});
+      userListByCondition({ 'vip_payed': true }, { 'username': 1 });
 
     }
 
-    $scope.vip_sort=function()
-    {
+    $scope.userListByConditionStatuses = function (statuses) {
       $scope.user_list = [];
-      userListByCondition({'vip_payed':true},{'vip_payed_time':-1});
+      userListByCondition({ 'vip_status': { $in: statuses } }, { 'vip_payed_time': -1 });
+    }
+
+
+    $scope.vip_sort = function () {
+      $scope.user_list = [];
+      userListByCondition({ 'vip_payed': true }, { 'vip_payed_time': -1 });
     }
 
     $scope.getVipStatus = function (status) {
