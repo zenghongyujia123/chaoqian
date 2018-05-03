05de44e9c27b6460ed678b95d5d4a4ebec4a4f2d
hardy <hardy@hardydeMacBook-Pro.local>
2018-05-03 13:52:23 +0800

    Update for test

--- web/c_wechat/controllers/page_wechat.client.controller.js
+++ web/c_wechat/controllers/page_wechat.client.controller.js
@@ -158,7 +158,7 @@ function onMenuShareAppMessage(title,desc,url,img) {
     title: title, // 分享标题
     desc: desc, // 分享描述
     link: url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-    imgUrl: img, // 分享图标
+    imgUrl: encodeURI( img), // 分享图标
     success: function () {
       alert('haha')
       // 用户确认分享后执行的回调函数
