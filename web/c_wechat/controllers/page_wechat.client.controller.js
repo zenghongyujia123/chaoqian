8f5e606f34f908d19ddbffa2230c1f2b0da2955c
hardy <hardy@hardydeMacBook-Pro.local>
2018-05-03 13:59:33 +0800

    Update for test

--- web/c_wechat/controllers/page_wechat.client.controller.js
+++ web/c_wechat/controllers/page_wechat.client.controller.js
@@ -153,12 +153,12 @@ function getUserJsApiTicket(url, callback) {
   });
 }
 
-function onMenuShareAppMessage(title,desc,url,img) {
+function onMenuShareAppMessage(title, desc, url, img) {
   wx.onMenuShareAppMessage({
     title: title, // 分享标题
     desc: desc, // 分享描述
     link: url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-    imgUrl: encodeURI( img), // 分享图标
+    imgUrl: encodeURI(img), // 分享图标
     success: function () {
       alert('haha')
       // 用户确认分享后执行的回调函数
@@ -169,6 +169,20 @@ function onMenuShareAppMessage(title,desc,url,img) {
       // 用户取消分享后执行的回调函数
     }
   });
+}
+function onMenuShareTimeline(title,url,img) {
+  wx.onMenuShareTimeline({
+    title: title, // 分享标题
+    link: url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+    imgUrl: img, // 分享图标
+    success: function () {
+      // 用户确认分享后执行的回调函数
+    },
+    cancel: function () {
+      // 用户取消分享后执行的回调函数
+    }
+  });
+
 }
 
 
