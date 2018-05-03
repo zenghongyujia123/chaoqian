6eaf2966378083acbcb9930aae5e5726a6da5b57
hardy <hardy@hardydeMacBook-Pro.local>
2018-05-03 13:49:50 +0800

    Update for share image

--- web/c_wechat/controllers/page_wechat.client.controller.js
+++ web/c_wechat/controllers/page_wechat.client.controller.js
@@ -153,12 +153,12 @@ function getUserJsApiTicket(url, callback) {
   });
 }
 
-function onMenuShareAppMessage(title,desc,url) {
+function onMenuShareAppMessage(title,desc,url,img) {
   wx.onMenuShareAppMessage({
     title: title, // 分享标题
     desc: desc, // 分享描述
     link: url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-    imgUrl: 'http://img.taopic.com/uploads/allimg/121014/234931-1210140JK414.jpg', // 分享图标
+    imgUrl: img, // 分享图标
     success: function () {
       alert('haha')
       // 用户确认分享后执行的回调函数
