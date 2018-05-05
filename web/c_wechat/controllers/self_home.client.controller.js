09d6c305b67baffc4e9fa958c8990f0bb6b65127
hardy <hardy@hardydeMacBook-Pro.local>
2018-05-05 15:33:33 +0800

    Clear code

--- web/c_wechat/controllers/self_home.client.controller.js
+++ web/c_wechat/controllers/self_home.client.controller.js
@@ -38,32 +38,32 @@ $(function () {
     window.location = '/page_wechat/self_home?' + arr.join('&');
   });
 
-  function updateUserLocation(lng, lat) {
-    $.ajax({
-      url: '/user/updateUserLocation',
-      method: 'post',
-      data: {
-        lng: lng,
-        lat: lat
-      },
-      success: function (data) {
-      }
-    });
-  }
+  // function updateUserLocation(lng, lat) {
+  //   $.ajax({
+  //     url: '/user/updateUserLocation',
+  //     method: 'post',
+  //     data: {
+  //       lng: lng,
+  //       lat: lat
+  //     },
+  //     success: function (data) {
+  //     }
+  //   });
+  // }
 
-  getUserJsApiTicket(window.location.href,function (data) {
-    wx.getLocation({
-      type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-      success: function (res) {
-        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-        var speed = res.speed; // 速度，以米/每秒计
-        var accuracy = res.accuracy; // 位置精度
-        if (longitude > 0 && latitude > 0) {
-          updateUserLocation(longitude, latitude);
-        }
-      }
-    });
+  // getUserJsApiTicket(window.location.href,function (data) {
+  //   wx.getLocation({
+  //     type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+  //     success: function (res) {
+  //       var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+  //       var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+  //       var speed = res.speed; // 速度，以米/每秒计
+  //       var accuracy = res.accuracy; // 位置精度
+  //       if (longitude > 0 && latitude > 0) {
+  //         updateUserLocation(longitude, latitude);
+  //       }
+  //     }
+  //   });
   
-  });
+  // });
 });
