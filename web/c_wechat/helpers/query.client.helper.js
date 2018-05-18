ef414c21b233fdbb23e9928f195b169580255ba8
hardy <hardy@hardydeMacBook-Pro.local>
2018-05-18 17:13:59 +0800

    Update for test

--- web/c_wechat/helpers/query.client.helper.js
+++ web/c_wechat/helpers/query.client.helper.js
@@ -25,41 +25,47 @@ function ge_ren_hei_ming_dan(data, callback) {
   if (localStorage.getItem("device") === 'native') {
     return window.location = '/lianlian_pay/page_lianlian?pay_type=query_黑灰行为';
   }
-  get_pre_pay_id('query_黑灰行为', function () {
-    $.ajax({
-      url: '/third_query/ge_ren_hei_ming_dan',
-      method: 'post',
-      data: {
-        bankCardNo: data.backnumber,
-        idCard: data.idcard,
-        mobile: data.phone,
-        name: data.name,
-      },
-      success: function (data) {
-        console.log(data);
-        return callback(data);
-      }
+  else{
+    get_pre_pay_id('query_黑灰行为', function () {
+      $.ajax({
+        url: '/third_query/ge_ren_hei_ming_dan',
+        method: 'post',
+        data: {
+          bankCardNo: data.backnumber,
+          idCard: data.idcard,
+          mobile: data.phone,
+          name: data.name,
+        },
+        success: function (data) {
+          console.log(data);
+          return callback(data);
+        }
+      });
     });
-  });
+  }
+  
 }
 
 function hei_zhong_jie(data, callback) {
   if (localStorage.getItem("device") === 'native') {
     return window.location = '/lianlian_pay/page_lianlian?pay_type=query_黑中介';
   }
-  get_pre_pay_id('query_黑中介', function () {
-    $.ajax({
-      url: '/third_query/hei_zhong_jie',
-      method: 'post',
-      data: {
-        mobile: data.phone
-      },
-      success: function (data) {
-        console.log(data);
-        return callback(data);
-      }
+  else{
+    get_pre_pay_id('query_黑中介', function () {
+      $.ajax({
+        url: '/third_query/hei_zhong_jie',
+        method: 'post',
+        data: {
+          mobile: data.phone
+        },
+        success: function (data) {
+          console.log(data);
+          return callback(data);
+        }
+      });
     });
-  });
+  }
+ 
 }
 
 
