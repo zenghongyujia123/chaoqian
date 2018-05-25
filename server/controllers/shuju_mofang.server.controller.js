e9d916824f2d5da809ed2c09826a0eaea62008ff
hardy <hardy@hardydeMacBook-Pro.local>
2018-05-25 22:22:47 +0800

    Update for test

--- server/controllers/shuju_mofang.server.controller.js
+++ server/controllers/shuju_mofang.server.controller.js
@@ -25,7 +25,7 @@ exports.shuju_mofang_callback_taobao = function (req, res, next) {
 exports.shuju_mofang_page_taobao = function (req, res, next) {
   console.log(req.params);
   var url = 'https://open.shujumohe.com/box/ds/000103?box_token=BE7A576504424A2494EF3633F7110B50&cb=' +
-    encodeURIComponent('http://www.chaoqianwang.com/shuju_mofang_page_taobao') + '&passback_params=' + req.user._id.toString();
+    encodeURIComponent('http://www.chaoqianwang.com/shuju_mofang_callback_taobao') + '&passback_params=' + req.user._id.toString();
   return res.redirect(url);
   // return res.redirect('/page_wechat/vip_auth_info?user_id=' + req.body.passback_params);
 };
