47d6ccfee41423405efd9d5c8a97503b373de64f
hardy <hardy@hardydeMacBook-Pro.local>
2018-05-25 22:15:34 +0800

    Update for test

---
+++ server/controllers/shuju_mofang.server.controller.js
@@ -0,0 +1,34 @@
+/**
+ * Created by zenghong on 2017/8/8.
+ */
+var userLogic = require('./../logics/user');
+var path = require('path');
+var agent = require('superagent').agent();
+
+
+exports.shuju_mofang_taobao = function (req, res, next) {
+  console.log(req.query);
+  console.log(req.body);
+  console.log(req.params);
+  // return res.redirect('/page_wechat/vip_auth_info?user_id=' + req.body.passback_params);
+};
+
+exports.shuju_mofang_callback_taobao = function (req, res, next) {
+  console.log(req.query);
+  console.log(req.body);
+  console.log(req.params);
+  return redirect('/page_wechat/vip_auth_info');
+  // return res.redirect('/page_wechat/vip_auth_info?user_id=' + req.body.passback_params);
+};
+
+
+exports.shuju_mofang_page_taobao = function (req, res, next) {
+  console.log(req.params);
+  var url = 'https://open.shujumohe.com/box/ds/000103?box_token=BE7A576504424A2494EF3633F7110B50&cb=' +
+    encodeURIComponent('http://www.chaoqianwang.com/shuju_mofang_page_taobao') + '&passback_params=' + req.user._id.toString();
+  return res.redirect(url);
+  // return res.redirect('/page_wechat/vip_auth_info?user_id=' + req.body.passback_params);
+};
+
+
+
---
+++ server/routes/shuju_mofang.server.route.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zenghong on 2017/8/8.
+ */
+
+var pbcController = require('../controllers/shuju_mofang');
+var userFilter = require('../filters/user');
+
+module.exports = function (app) {
+  app.route('/shuju_mofang_callback_taobao').get(pbcController.shuju_mofang_callback_taobao);
+  app.route('/shuju_mofang_page_taobao').get(userFilter.requireUser, pbcController.shuju_mofang_page_taobao);
+};
