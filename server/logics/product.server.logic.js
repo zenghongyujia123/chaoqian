c72bd9f1dcdbfb6cd2dcb3a8e533af4e23d5f7cd
zenghongyujia123 <1963968619@qq.com>
2018-06-12 18:12:44 +0800

    Update for test

--- server/logics/product.server.logic.js
+++ server/logics/product.server.logic.js
@@ -10,8 +10,8 @@ var sysErr = require('./../errors/system');
 var that = exports;
 
 
-exports.getProductShareUrl = function (url, callback) {
-  var url = 'http://www.chaoqianwang.com/page_h5/third_page?url=' + url;
+exports.getProductShareUrl = function (productInfo, callback) {
+  var url = 'http://www.chaoqianwang.com/page_h5/third_page?url=' + productInfo.organization_url + '&product_name=' + productInfo.name;
   agent.get('http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long=' + url)
     .end(function (err, data) {
       return callback(err, data.body);
