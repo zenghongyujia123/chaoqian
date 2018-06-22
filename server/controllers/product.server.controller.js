4dd0518b170b803285868bb2ba901995ec1bebaf
zenghongyujia123 <1963968619@qq.com>
2018-06-22 13:09:08 +0800

    Update for etst

--- server/controllers/product.server.controller.js
+++ server/controllers/product.server.controller.js
@@ -34,8 +34,6 @@ exports.productDetail = function (req, res, next) {
 exports.product_history_list = function (req, res, next) {
   var today = new Date(new Date().setHours(0, 0, 0, 0));
   var yestoday = new Date(end).setDate(end.getDate() - 1)
-  console.log(start);
-  console.log(end);
   productLogic.product_history_list({}, function (err, total_result) {
     productLogic.product_history_list({ start_time: yestoday, end_time: today }, function (err, yestoday_result) {
       productLogic.product_history_list({ start_time: today }, function (err, today_result) {
