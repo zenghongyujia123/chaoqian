2ce913270406a45f039fbcf5ed4ad522815feb72
zenghongyujia123 <1963968619@qq.com>
2018-06-24 15:36:48 +0800

    Update for test

--- server/controllers/page_index.server.controller.js
+++ server/controllers/page_index.server.controller.js
@@ -4,6 +4,7 @@
 var path = require('path');
 var productLogic = require('../logics/product');
 var articleLogic = require('../logics/article');
+var jietiaoLogic = require('../logics/jietiao');
 var userLogic = require('../logics/user');
 var productFilterloigc = require('../logics/product_filter');
 var provinces = require('../constants/city');
@@ -23,36 +24,44 @@ exports.partner_main = function (req, res, next) {
 
 
 exports.index = function (req, res, next) {
-  console.log(req.headers.host);
+  var filepath = path.join(__dirname, '../../web/c_platform/views/home.client.view.html');
+  if (req.headers.host === 'm.chaoqianwang.com') {
+    filepath = path.join(__dirname, '../../web/c_platform/views/home_mip.client.view.html');
+  }
+
   req.cookies.city = req.params.city || req.cookies.city || '';
   cookieLib.setCookie(res, 'city', req.cookies.city);
   productLogic.productList({ sort_key: 'update_time', sort_value: -1 }, function (err, products) {
-    var filepath = path.join(__dirname, '../../web/c_platform/views/home.client.view.html');
     articleLogic.articleList({}, function (err, articles) {
-      if (req.headers.host === 'm.chaoqianwang.com') {
-        filepath = path.join(__dirname, '../../web/c_platform/views/home_mip.client.view.html');
-      }
-      return res.render(filepath, {
-        city: req.cookies.city || '',
-        articles: articles || [],
-        products: products || [],
-        device: req.cookies.device || ''
-      });
+      jietiaoLogic.jietiaoList({}, function (err, jietiaos) {
+        return res.render(filepath, {
+          city: req.cookies.city || '',
+          articles: articles || [],
+          products: products || [],
+          jietiaos: jietiaos || [],
+          device: req.cookies.device || ''
+        });
+
+      })
     });
   });
 };
 
 exports.home_mip = function (req, res, next) {
+  var filepath = path.join(__dirname, '../../web/c_platform/views/home_mip.client.view.html');
+
   req.cookies.city = req.params.city || req.cookies.city || '';
   cookieLib.setCookie(res, 'city', req.cookies.city);
   productLogic.productList({}, function (err, products) {
-    var filepath = path.join(__dirname, '../../web/c_platform/views/home_mip.client.view.html');
     articleLogic.articleList({}, function (err, articles) {
-      return res.render(filepath, {
-        city: req.cookies.city || '',
-        articles: articles || [],
-        products: products || [],
-        device: req.cookies.device || ''
+      jietiaoLogic.jietiaoList({}, function (err, jietiaos) {
+        return res.render(filepath, {
+          city: req.cookies.city || '',
+          articles: articles || [],
+          products: products || [],
+          jietiaos: jietiaos || [],
+          device: req.cookies.device || ''
+        });
       });
     });
   });
@@ -86,13 +95,13 @@ exports.product_detail = function (req, res, next) {
       articleLogic.articleListByIds(product.article_list, function (err, article_list) {
         articleLogic.articleSingleList({}, function (err, article_single_list) {
           articleLogic.increase_read_count(article_list[0]._id, function () {
-            return res.render(filepath, {city: req.cookies.city || '', product: product, article: article_list[0], article_single_list: article_single_list });
+            return res.render(filepath, { city: req.cookies.city || '', product: product, article: article_list[0], article_single_list: article_single_list });
           });
         })
       });
     }
     else {
-      return res.render(filepath, {city: req.cookies.city || '', product: null, article: {}, article_single_list: [] });
+      return res.render(filepath, { city: req.cookies.city || '', product: null, article: {}, article_single_list: [] });
     }
 
   });
@@ -107,7 +116,7 @@ exports.article_detail = function (req, res, next) {
   articleLogic.increase_read_count(req.params.article_id, function () {
     articleLogic.articleDetail(req.params.article_id, function (err, article) {
       articleLogic.articleSingleList({}, function (err, article_single_list) {
-        return res.render(filepath, { city: req.cookies.city || '',product: null, article: article, article_single_list: article_single_list });
+        return res.render(filepath, { city: req.cookies.city || '', product: null, article: article, article_single_list: article_single_list });
       });
     });
   })
